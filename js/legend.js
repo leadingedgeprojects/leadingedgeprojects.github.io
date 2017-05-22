a383ffe5ca4eb95b5b1ac27f77ed0fc1516a71a8
Kumar Utkarsh <kumarutkarsh.ingen@gmail.com>
2017-05-22 14:47:59 +0530

    [ADD] First Commit

---
+++ js/legend.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#div-about").click(function() {
+		window.location = 'about.html';
+	});
+	$("#div-internships").click(function() {
+		window.location = 'internships.html';
+	});
+	$("#div-courses").click(function() {
+		window.location = 'courses.html';
+	});
+	$("#div-projects").click(function() {
+		window.location = 'projects.html';
+	});
+});
+
